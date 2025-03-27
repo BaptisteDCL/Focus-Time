e2b96551d9318058f18821f5305fe1c0a70e22fd
BaptisteDCL <baptiste.duclos2@gmail.com>
2025-03-27 21:42:57 +0100

    Début de la partie Front; Problème Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind fonctionne 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
